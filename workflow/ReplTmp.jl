b689aeed520631d47b1df5fc3e877c33d953638f
phasetr <phasetr@gmail.com>
2024-08-24 14:06:05 +0900

    commit

--- workflow/ReplTmp.jl
+++ workflow/ReplTmp.jl
@@ -5,6 +5,7 @@ export say_hello2
 say_hello() = println("Hello!")
 say_hello2() = println("Hello2!")
 say_hello3() = println("Hello3!")
+isodd(x) = x % 2 == 1
 
 # your other definitions here
 
--- workflow/tst.jl
+++ workflow/tst.jl
@@ -1,13 +1,33 @@
+using Test
 include("ReplTmp.jl")
 import .ReplTmp
 # using .Tmp # we can use `using` to bring the exported symbols in `Tmp` into our namespace
 # say_hello()
 
-# say_hello
-ReplTmp.say_hello()
-# say_hello2
-ReplTmp.say_hello2()
-# say_hello3
-ReplTmp.say_hello3()
+@testset "1st test set" begin
+    @testset "Inside 1st test set" begin
+      @test 1 + 1 == 2
+      @test_throws DomainError sqrt(-1)
+  
+      @test isapprox(0.1 + 0.2, 0.3)  # 浮動小数点数の比較
+      @test [1, 2, 3] ≈ [1, 2, 3]     # 配列の比較  
+    end
 
-# your other test code here
+    @testset "Inside 2nd test set" begin
+      @test ReplTmp.isodd(1) == true
+      # say_hello
+      ReplTmp.say_hello()
+      # say_hello2
+      ReplTmp.say_hello2()
+      # say_hello3
+      ReplTmp.say_hello3()
+
+      @test ReplTmp.isodd(2) == false
+      @test ReplTmp.isodd(3) == true
+      # your other test code here
+    end
+
+    @testset "Parameterized test $i" for i in 1:3
+      @test i < 4
+    end
+end
